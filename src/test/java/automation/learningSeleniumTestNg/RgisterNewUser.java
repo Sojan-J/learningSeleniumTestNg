e9c69656101cf6844a896febbe7f08202038e1f2
sojan Jose <sojanjose@ymail.com>
2023-06-14 10:31:01 -0400

    second commit

--- src/test/java/automation/learningSeleniumTestNg/RgisterNewUser.java
+++ src/test/java/automation/learningSeleniumTestNg/RgisterNewUser.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.NewSessionPayload;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -17,24 +18,35 @@ import org.testng.annotations.Test;
 public class RgisterNewUser {
 	WebDriver driver;
 	WebDriverWait wait;
+	String firstName="";
+	String lastName="";
+	String newCustomerEmail="";
+	String newPassword="";
+	
 
 	@BeforeMethod
-	public void launchBrowser() {
+	public void startUp() {
 		System.setProperty("webdriver.chrome.driver", "W:\\Driver\\Chrome\\chromedriver.exe");
 		driver = new ChromeDriver();
 		wait = new WebDriverWait(driver, 10);
 		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=common/home");
 		driver.manage().window().maximize();
+		 firstName=UtilityClass.generateRandomName();
+	     lastName=UtilityClass.generateRandomName();
+		 newCustomerEmail=lastName+firstName+"@ymail.com";
+		 newPassword=UtilityClass.generateRandomName()+"123";
+		 
+		
 	}
-
-	@AfterMethod
-	public void closeBrowser() {
+                            
+	/*@AfterMethod
+	public void tearDown() {
 		driver.close();
-	}
+	}*/
 
 	@Test
 	// Verify new registration
-	public void tc001() {
+	public void validateIfNewuserIsRgistered() {
 		WebElement myAccount = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='My Account']"))));
 		myAccount.click();
@@ -42,17 +54,17 @@ public class RgisterNewUser {
 				driver.findElement(By.cssSelector("ul.dropdown-menu.dropdown-menu-right > li:nth-child(1) > a"))));
 		registerBtn.click();
 		WebElement firstnameInput = driver.findElement(By.cssSelector("input#input-firstname"));
-		firstnameInput.sendKeys("Scott");
+		firstnameInput.sendKeys(firstName);
 		WebElement lastNameInput = driver.findElement(By.cssSelector("input#input-lastname"));
-		lastNameInput.sendKeys("William");
+		lastNameInput.sendKeys(lastName);
 		WebElement emailInput = driver.findElement(By.cssSelector("input#input-email"));
-		emailInput.sendKeys("william@scott.com1245");
+		emailInput.sendKeys(newCustomerEmail);
 		WebElement telePhone = driver.findElement(By.id("input-telephone"));
 		telePhone.sendKeys("1234567");
 		WebElement passwordInput = driver.findElement(By.id("input-password"));
-		passwordInput.sendKeys("password123");
+		passwordInput.sendKeys(newPassword);
 		WebElement confirmPasswordInput = driver.findElement(By.id("input-confirm"));
-		confirmPasswordInput.sendKeys("password123");
+		confirmPasswordInput.sendKeys(newPassword);
 
 		WebElement subscribeYes = driver.findElement(By.xpath(" (//input[@name='newsletter'])[1]"));
 		WebElement subscribeNo = driver.findElement(By.xpath(" (//input[@name='newsletter'])[2]"));
@@ -61,28 +73,13 @@ public class RgisterNewUser {
 		agreeCheck.click();
 		WebElement continueButton = driver.findElement(By.cssSelector(" input[value='Continue']"));
 		continueButton.click();
-		Assert.assertEquals(driver.getTitle(), "Your Account Has Been Created");
-	}
-	@Test
-	// verify email format
-	public void tc002() {
-		WebElement myAccount = wait
-				.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='My Account']"))));
-		myAccount.click();
-		WebElement registerBtn = wait.until(ExpectedConditions.visibilityOf(
-				driver.findElement(By.cssSelector("ul.dropdown-menu.dropdown-menu-right > li:nth-child(1) > a"))));
-		registerBtn.click();
-		WebElement emailInput = wait
-				.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("input#input-email"))));
-		emailInput.sendKeys("williamscottcom1245");
-		// wait.until(ExpectedConditions.alertIsPresent());
-		Assert.assertTrue(isalertPresent(), "Email format warning is not present");
-
+		Assert.assertEquals(driver.getTitle(), "Your Account Has Been Created!");
 	}
 
+	
 	@Test
 	// verify if password entered has more than 3 chars
-	public void tc003() {
+	public void validatePasswordHasMorethan3Chars() {
 		WebElement myAccount = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='My Account']"))));
 		myAccount.click();
@@ -90,11 +87,11 @@ public class RgisterNewUser {
 				driver.findElement(By.cssSelector("ul.dropdown-menu.dropdown-menu-right > li:nth-child(1) > a"))));
 		registerBtn.click();
 		WebElement firstnameInput = driver.findElement(By.cssSelector("input#input-firstname"));
-		firstnameInput.sendKeys("Scott");
+		firstnameInput.sendKeys(firstName);
 		WebElement lastNameInput = driver.findElement(By.cssSelector("input#input-lastname"));
-		lastNameInput.sendKeys("William");
+		lastNameInput.sendKeys(lastName);
 		WebElement emailInput = driver.findElement(By.cssSelector("input#input-email"));
-		emailInput.sendKeys("william@scott.com1000");
+		emailInput.sendKeys(newCustomerEmail);
 		WebElement telePhone = driver.findElement(By.id("input-telephone"));
 		telePhone.sendKeys("1234567");
 		WebElement passwordInput = driver.findElement(By.id("input-password"));
@@ -109,18 +106,14 @@ public class RgisterNewUser {
 		agreeCheck.click();
 		WebElement continueButton = driver.findElement(By.cssSelector(" input[value='Continue']"));
 		continueButton.click();
-		try {
-			Assert.assertTrue(
-					driver.findElement(By.xpath("//div[text()='Password must be between 4 and 20 characters!']"))
-							.isDisplayed());
-		} catch (NoSuchElementException ex) {
-			Assert.assertTrue(false, " warning  is not given when password entered is less than 4 chars");
-		}
+
+		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Password must be between 4 and 20 characters!']"))
+				.isDisplayed(), "if pass word is less than 4 chars warning not given");
 	}
 
 	@Test
 	// verify user gets warning when confirm password is not matching
-	public void tc004() {
+	public void validateIfuserGetWarningWhenConfirmPasswordNotMatching() {
 		WebElement myAccount = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='My Account']"))));
 		myAccount.click();
@@ -128,15 +121,15 @@ public class RgisterNewUser {
 				driver.findElement(By.cssSelector("ul.dropdown-menu.dropdown-menu-right > li:nth-child(1) > a"))));
 		registerBtn.click();
 		WebElement firstnameInput = driver.findElement(By.cssSelector("input#input-firstname"));
-		firstnameInput.sendKeys("Scott");
+		firstnameInput.sendKeys(firstName);
 		WebElement lastNameInput = driver.findElement(By.cssSelector("input#input-lastname"));
-		lastNameInput.sendKeys("William");
+		lastNameInput.sendKeys(lastName);
 		WebElement emailInput = driver.findElement(By.cssSelector("input#input-email"));
-		emailInput.sendKeys("william@scott.com1000");
+		emailInput.sendKeys(newCustomerEmail);
 		WebElement telePhone = driver.findElement(By.id("input-telephone"));
 		telePhone.sendKeys("1234567");
 		WebElement passwordInput = driver.findElement(By.id("input-password"));
-		passwordInput.sendKeys("pas1234");
+		passwordInput.sendKeys(newPassword);
 		WebElement confirmPasswordInput = driver.findElement(By.id("input-confirm"));
 		confirmPasswordInput.sendKeys("pas123");
 
@@ -147,20 +140,15 @@ public class RgisterNewUser {
 		agreeCheck.click();
 		WebElement continueButton = driver.findElement(By.cssSelector(" input[value='Continue']"));
 		continueButton.click();
-		try {
-			Assert.assertTrue(
-					driver.findElement(By.xpath("//div[text()='Password confirmation does not match password!']"))
-							.isDisplayed(),
-					"Warning is not present");
-		} catch (NoSuchElementException ex) {
-			Assert.assertTrue(false, "Warning is not present when user enter miss matched password");
-		}
+
+		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Password confirmation does not match password!']"))
+				.isDisplayed(), "Warning is not present");
 
 	}
 
 	@Test
 	// Verify if user is able to register and login back.
-	public void tc005() {
+	public void validateIfUserIsAbleRegisterAndThenLogin() {
 		WebElement myAccount = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='My Account']"))));
 		myAccount.click();
@@ -168,17 +156,17 @@ public class RgisterNewUser {
 				driver.findElement(By.cssSelector("ul.dropdown-menu.dropdown-menu-right > li:nth-child(1) > a"))));
 		registerBtn.click();
 		WebElement firstnameInput = driver.findElement(By.cssSelector("input#input-firstname"));
-		firstnameInput.sendKeys("Scott");
+		firstnameInput.sendKeys(firstName);
 		WebElement lastNameInput = driver.findElement(By.cssSelector("input#input-lastname"));
-		lastNameInput.sendKeys("Davis");
+		lastNameInput.sendKeys(lastName);
 		WebElement emailInput = driver.findElement(By.cssSelector("input#input-email"));
-		emailInput.sendKeys("davis20@scott.com1245");
+		emailInput.sendKeys(newCustomerEmail);
 		WebElement telePhone = driver.findElement(By.id("input-telephone"));
 		telePhone.sendKeys("1234567");
 		WebElement passwordInput = driver.findElement(By.id("input-password"));
-		passwordInput.sendKeys("password123");
+		passwordInput.sendKeys(newPassword);
 		WebElement confirmPasswordInput = driver.findElement(By.id("input-confirm"));
-		confirmPasswordInput.sendKeys("password123");
+		confirmPasswordInput.sendKeys(newPassword);
 
 		WebElement subscribeYes = driver.findElement(By.xpath(" (//input[@name='newsletter'])[1]"));
 		WebElement subscribeNo = driver.findElement(By.xpath(" (//input[@name='newsletter'])[2]"));
@@ -197,10 +185,10 @@ public class RgisterNewUser {
 		loginBtn.click();
 		WebElement loginEmailInput = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("input-email"))));
-		loginEmailInput.sendKeys("davis20@scott.com1245");
+		loginEmailInput.sendKeys(newCustomerEmail);
 		WebElement loginPasswordInput = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#input-password"))));
-		loginPasswordInput.sendKeys("password123");
+		loginPasswordInput.sendKeys(newPassword);
 		WebElement loginSubmitBtn = driver.findElement(By.xpath("//input[@value='Login']"));
 		loginSubmitBtn.submit();
 		Assert.assertEquals(driver.getTitle(), "My Account");
@@ -209,7 +197,7 @@ public class RgisterNewUser {
 	
 	@Test
 	// verify user not registered unless privacy policy is checked
-	public void tc006() {
+	public void validateUserNotRegisteredUnlessPrivacyPolicyIsChecked() {
 		WebElement myAccount = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='My Account']"))));
 		myAccount.click();
@@ -217,35 +205,34 @@ public class RgisterNewUser {
 				driver.findElement(By.cssSelector("ul.dropdown-menu.dropdown-menu-right > li:nth-child(1) > a"))));
 		registerBtn.click();
 		WebElement firstnameInput = driver.findElement(By.cssSelector("input#input-firstname"));
-		firstnameInput.sendKeys("Scott");
+		firstnameInput.sendKeys(firstName);
 		WebElement lastNameInput = driver.findElement(By.cssSelector("input#input-lastname"));
-		lastNameInput.sendKeys("William");
+		lastNameInput.sendKeys(lastName);
 		WebElement emailInput = driver.findElement(By.cssSelector("input#input-email"));
-		emailInput.sendKeys("william@scott.com1245");
+		emailInput.sendKeys(newCustomerEmail);
 		WebElement telePhone = driver.findElement(By.id("input-telephone"));
 		telePhone.sendKeys("1234567");
 		WebElement passwordInput = driver.findElement(By.id("input-password"));
-		passwordInput.sendKeys("password123");
+		passwordInput.sendKeys(newPassword);
 		WebElement confirmPasswordInput = driver.findElement(By.id("input-confirm"));
-		confirmPasswordInput.sendKeys("password123");
+		confirmPasswordInput.sendKeys(newPassword);
 
 		WebElement subscribeYes = driver.findElement(By.xpath(" (//input[@name='newsletter'])[1]"));
 		WebElement subscribeNo = driver.findElement(By.xpath(" (//input[@name='newsletter'])[2]"));
 		subscribeNo.click();
 		WebElement continueButton = driver.findElement(By.cssSelector(" input[value='Continue']"));
 		continueButton.click();
-		try {
+		
 			Assert.assertTrue(
-					driver.findElement(By.cssSelector("div.alert.alert-danger.alert-dismissible")).isDisplayed());
-		} catch (NoSuchElementException ex) {
-			Assert.assertTrue(false, "no warning is given when privacy policy is unchecked");
-		}
+					driver.findElement(By.cssSelector("div.alert.alert-danger.alert-dismissible")).isDisplayed(),"user able to register with unchecked privacy policy");
+		} 
+			
 
-	}
+	
 
 	@Test
 	// validate user is not able use existing email id to register
-	public void tc007() {
+	public void validateIfUserNotAbleToUseExistingEmailToRegister() {
 		WebElement myAccount = wait
 				.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='My Account']"))));
 		myAccount.click();
@@ -272,24 +259,10 @@ public class RgisterNewUser {
 		agreeCheck.click();
 		WebElement continueButton = driver.findElement(By.cssSelector(" input[value='Continue']"));
 		continueButton.click();
-		try {
+		
 			Assert.assertTrue(
-					driver.findElement(By.cssSelector("div.alert.alert-danger.alert-dismissible")).isDisplayed());
-		} catch (NoSuchElementException ex) {
-			Assert.assertTrue(
-					driver.findElement(By.cssSelector("div.alert.alert-danger.alert-dismissible")).isDisplayed(),
-					"No warning given when existing email is used");
-		}
-	}
-	
-	
-	public boolean isalertPresent() {
-		try {
-			driver.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException Ex) {
-			return false;
-		}
+					driver.findElement(By.cssSelector("div.alert.alert-danger.alert-dismissible")).isDisplayed(),"user is able use existing email id to register");
+		
 	}
 
 }
